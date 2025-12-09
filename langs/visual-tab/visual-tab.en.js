235d3c42366e2abcb15e4aaa0f8bfbc804d1712e
z3nth10n <z3nth10n@gmail.com>
2025-12-09 18:24:46 +0100

    remove visual-tab.js from langs

--- langs/visual-tab/visual-tab.en.js
+++ langs/visual-tab/visual-tab.en.js
@@ -1,11 +1,12 @@
 window.translations_en = {
-  "tool_visual_tab_title": "Visual Tab Player",
-  "tool_visual_tab_desc": "Visualize your favorite tabs in a new way.",
-  "loading": "Loading tabs...",
-  "back_to_list": "Back to List",
-  "legend_note": "Note",
-  "legend_chord": "Chord",
-  "select_artist": "Select Artist",
-  "unknown_artist": "Unknown Artist",
-  "unknown_song": "Unknown Song"
+  tool_visual_tab_title: "Visual Tab Player",
+  tool_visual_tab_desc: "Visualize your favorite tabs in a new way.",
+  loading: "Loading tabs...",
+  back_to_list: "Back to List",
+  legend_note: "Note",
+  legend_chord: "Chord",
+  select_artist: "Select Artist",
+  unknown_artist: "Unknown Artist",
+  unknown_song: "Unknown Song",
 };
+
--- langs/visual-tab/visual-tab.es.js
+++ langs/visual-tab/visual-tab.es.js
@@ -1,11 +1,12 @@
 window.translations_es = {
-  "tool_visual_tab_title": "Reproductor Visual de Tabs",
-  "tool_visual_tab_desc": "Visualiza tus tablaturas favoritas de una forma nueva.",
-  "loading": "Cargando tablaturas...",
-  "back_to_list": "Volver a la lista",
-  "legend_note": "Nota",
-  "legend_chord": "Acorde",
-  "select_artist": "Seleccionar Artista",
-  "unknown_artist": "Artista Desconocido",
-  "unknown_song": "Canción Desconocida"
+  tool_visual_tab_title: "Reproductor Visual de Tabs",
+  tool_visual_tab_desc:
+    "Visualiza tus tablaturas favoritas de una forma nueva.",
+  loading: "Cargando tablaturas...",
+  back_to_list: "Volver a la lista",
+  legend_note: "Nota",
+  legend_chord: "Acorde",
+  select_artist: "Seleccionar Artista",
+  unknown_artist: "Artista Desconocido",
+  unknown_song: "Canción Desconocida",
 };
